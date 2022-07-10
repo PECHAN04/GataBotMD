7c5f0aa2ea420da67e38737fe92468cdd13302b8
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-10 11:09:06 -0500

    Update so-abduzcan_mp3.js

--- plugins/so-abduzcan_mp3.js
+++ plugins/so-abduzcan_mp3.js
@@ -6,8 +6,8 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
-conn.sendFile(m.chat, vn, 'abduzcan.mp3', null, m, true, {
+let vn = './media/tameimpala.mp3'
+conn.sendFile(m.chat, vn, 'tameimpala.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
