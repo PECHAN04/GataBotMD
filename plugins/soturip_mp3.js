b2e21694112eb3181ab9d10b83311b8d61bfc7f8
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-12 19:35:27 -0500

    Update soturip_mp3.js

--- plugins/soturip_mp3.js
+++ plugins/soturip_mp3.js
@@ -12,6 +12,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /turip|Turip|ip ip|Turip / 
+handler.customPrefix = /turip|Turip|ip|Turip / 
 handler.command = new RegExp
 export default handler
