73a6c4f6a8394b93251e83b55f67a5a1554069c8
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-19 22:33:09 -0500

    Update so-15okaerionichan.js

--- plugins/so-15okaerionichan.js
+++ plugins/so-15okaerionichan.js
@@ -6,12 +6,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
-conn.sendFile(m.chat, vn, 'abduzcan.mp3', null, m, true, {
+let vn = './media/okaerionichan.mp3'
+conn.sendFile(m.chat, vn, 'okaerionichan.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /abduzcan|Abduzcan|adbuzcan|Adbuzcan/ 
+handler.customPrefix = /volvi|Volvi|Volví|volví|Regrese|regrese/ 
 handler.command = new RegExp
 export default handler
