ea97e13523637be1168f762d9ebc36b93b5b870c
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-19 22:48:15 -0500

    Update so-18turip.js

--- plugins/so-18turip.js
+++ plugins/so-18turip.js
@@ -6,12 +6,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/turipp.mp3'
-conn.sendFile(m.chat, vn, 'turiipp.mp3', null, m, true, {
+let vn = './media/mirada.mp3'
+conn.sendFile(m.chat, vn, 'mirada.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Turip|turip|ip|Adbuzcan/ 
+handler.customPrefix = /d014|D015|d15|D15/ 
 handler.command = new RegExp
 export default handler
