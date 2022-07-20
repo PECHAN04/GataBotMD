1827ffc86649b7731e3e8b0a198d0fd4c71ab60b
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-19 22:27:52 -0500

    Update so-12muerenpormi.js

--- plugins/so-12muerenpormi.js
+++ plugins/so-12muerenpormi.js
@@ -6,12 +6,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
-conn.sendFile(m.chat, vn, 'abduzcan.mp3', null, m, true, {
+let vn = './media/mueren_pormi.mp3'
+conn.sendFile(m.chat, vn, 'mueren_pormi.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /abduzcan|Abduzcan|adbuzcan|Adbuzcan/ 
+handler.customPrefix = /d12|D12|d012|D012/ 
 handler.command = new RegExp
 export default handler
