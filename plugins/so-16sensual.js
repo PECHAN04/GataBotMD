52fc501e84f0c59b73dfebe6fbac33cd81cf2461
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-19 22:56:59 -0500

    Update so-16sensual.js

--- plugins/so-16sensual.js
+++ plugins/so-16sensual.js
@@ -6,7 +6,7 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
+let vn = './media/sensual.mp3'
 conn.sendFile(m.chat, vn, 'sensual.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
